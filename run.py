38cef1d357cf06cf8cbff2837a66d55fbbf4ab95
dead-programmer <yw0017@uah.edu>
2019-10-28 17:03:11 -0500

    delete champ by -champ_name

--- run.py
+++ run.py
@@ -2,15 +2,22 @@ import calculate
 import team_comps
 
 
-
 user_input = []
 
+
 def get_user_input():
+    # user_input_string = input("What champions do you have?")
+    # team_comps.build_data(user_input_string)
+    # calculate.match(user_input, team_comps.tier_S)
+
     user_input_string = input("What champions do you have?")
-    team_comps.build_data(user_input_string)
+    if '-' in user_input_string:
+        team_comps.delete_champ(user_input_string)
+    else:
+        team_comps.build_data(user_input_string)
     calculate.match(user_input, team_comps.tier_S)
 
 
 while 1:
-    team_comps.get_data()
+    # team_comps.get_data()
     get_user_input()
--- team_comps.py
+++ team_comps.py
@@ -5,35 +5,47 @@ from pprint import pprint
 #         print("KEY = " + key)
 #     return tier_S
 
+
 def get_data():
     pprint(data)
     return data
 
+
 def build_data(input):
-    user_data.append(input)
-    print(user_data)
+    user_team.append(input)
+    print(user_team)
+
+
+def delete_champ(deletion_input):                            # -champion_name
+    champ_name_length = len(deletion_input) - 1              # length = len(champion_name)
+    user_team.remove(deletion_input[-champ_name_length:])    # remove("champion_name")
+    print(user_team)
+
 
 tier_S = []
-user_data = []
+user_team = []
 
+# TODO: use web scraper to pull the following data automatically
 data = {
-    'void_brawlers' : ['khazix',
-                 'blitzcrank',
-                 'reksai',
-                 'vi',
-                 'akali',
-                 'chogath',
-                 'jinx',
-                 'kaisa']
-     ,
+    'void_brawlers':
+        ['khazix',
+         'blitzcrank',
+         'reksai',
+         'vi',
+         'akali',
+         'chogath',
+         'jinx',
+         'kaisa']
+    ,
 
-    'imperial_knights' : ['darius',
-                    'garen',
-                    'mordekaiser',
-                    'poppy',
-                    'katarina',
-                    'draven',
-                    'sejuani',
-                    'kayle',
-                    'swain']
+    'imperial_knights':
+        ['darius',
+         'garen',
+         'mordekaiser',
+         'poppy',
+         'katarina',
+         'draven',
+         'sejuani',
+         'kayle',
+         'swain']
 }
