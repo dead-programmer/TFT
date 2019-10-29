5fc218c9214bc6b33e0dae08031381401647ed2a
dead-programmer <yw0017@uah.edu>
2019-10-29 08:53:41 -0500

    support adding/deleting multiple champions at one time

--- run.py
+++ run.py
@@ -7,10 +7,16 @@ user_input = []
 
 def get_user_input():
     user_input_string = input("What champions do you have?")
-    if '-' in user_input_string:
-        team_comps.delete_champ(user_input_string)
-    else:
-        team_comps.add_champ(user_input_string)
+    user_input_string = user_input_string.split(" ")
+
+    print(user_input_string)
+
+    for i in user_input_string:
+        if '-' in i:
+            team_comps.delete_champ(i)
+        else:
+            team_comps.add_champ(i)
+
     calculate.match(user_input, team_comps.tier_S)
 
 
