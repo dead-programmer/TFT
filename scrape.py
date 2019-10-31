dd4b82a7caa7a5aca73d6f95bedb13b9a03f7f97
pmarella <pranay.marella@adtran.com>
2019-10-31 10:59:30 -0600

    Update requirements.txt and scrape the rendered HTML

--- scrape.py
+++ scrape.py
@@ -1,6 +1,39 @@
-from requests_html import HTMLSession
+''' Lines 2-28 were taken from:
+https://stackoverflow.com/questions/37754138/how-to-render-html-with-pyqt5s-qwebengineview
+'''
+def render(url):
+    """Fully render HTML, JavaScript and all."""
+
+    import sys
+    from PyQt5.QtCore import QEventLoop,QUrl
+    from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+    class Render(QWebEngineView):
+        def __init__(self, url):
+            self.html = None
+            self.app = QApplication(sys.argv)
+            QWebEngineView.__init__(self)
+            self.loadFinished.connect(self._loadFinished)
+            self.load(QUrl(url))
+            while self.html is None:
+                self.app.processEvents(QEventLoop.ExcludeUserInputEvents | QEventLoop.ExcludeSocketNotifiers | QEventLoop.WaitForMoreEvents)
+            self.app.quit()
+
+        def _callable(self, data):
+            self.html = data
+            self.app.quit()
+
+        def _loadFinished(self, result):
+            self.page().toHtml(self._callable)
+
+    return Render(url).html
+
+
+import requests
 from bs4 import BeautifulSoup
 
-#improvements to this can be to use mulithreading
-
-url = 'https://tftactics.gg/tierlist/team-comps/'
+url = "https://tftactics.gg/tierlist/team-comps/"
+html = requests.get(url).text
+renderedHTML = render(url)
+soup = BeautifulSoup(renderedHTML)
