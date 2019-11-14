d4b23da03d0241c5cf5bec642e6093d5cb9e3d8e
bpenning <blake.pennington@adtran.com>
2019-11-14 18:03:23 +0000

    Tie logic for teams, adjusted print statements, Yingren skeleton infrastructure for score system

--- calculate.py
+++ calculate.py
@@ -1,10 +1,80 @@
+import team_comps
+
+teams = team_comps.team_data
+best_team = []
+unique_list = []
+hit_list = []
+best_score = 0
 
 
-def match(input_list, data):
-    score = 0
-    for champion in input_list:
-        for match in data:
-            if champion in match:
+def score_calculator(user_team):
+    global best_team
+    global best_score
+    team = {
+        'team_name': '',
+        'team_champ': []
+    }
+    for team_comp in teams:
+        score = 0
+        for champion in user_team:
+            if champion in teams[team_comp]:
                 score += 1
-                print(champion)
-                print(score)
+                print("champion in score calculator: ", champion)
+                print("score: ", score)
+
+                if score > best_score:
+                    team['team_champ'].append(champion)
+                    team['team_name'] = team_comp
+                    del best_team[:]
+                    best_team.append(team)
+                    best_score = score
+                    print(best_score)
+                elif score == best_score:
+                    found = False
+                    for best in best_team:
+                        if teams[team_comp] == best['team_name']:
+                            found = True
+                    if found is False:
+                        team['team_champ'].append(champion)
+                        team['team_name'] = team_comp
+                        best_team.append(team)
+
+                # elif score == team['team_score']:
+                #     team['team_score'] = score
+                #     team['team_champ'] += champion
+                #     team['team_name'] = team_comp
+                #     best_team += team
+                score = 0
+
+    # result = best_team + team
+    for best in best_team:
+        print(best['team_name'])
+    # return best_team
+    return best_score
+
+
+def match(input_list):
+    # Iterate through user inputs, then append matches to hit_list
+    for champion in input_list:
+        for match in teams:
+            if champion in teams[match]:
+                hit_list.append(match)
+    if hit_list:
+        # FIND UNIQUE
+        for hit in hit_list:
+            if hit not in unique_list:
+                unique_list.append(hit)
+
+        max_count = 0
+
+        # Logic for deciding ties between teams
+        for team in unique_list:
+            count = hit_list.count(team)
+            if count > max_count:
+                max_count = count
+                del best_team[:]
+                best_team.append(team)
+            elif count == max_count:
+                best_team.append(team)
+    return best_team
+
--- run.py
+++ run.py
@@ -8,16 +8,14 @@ user_input = []
 def get_user_input():
     user_input_string = input("What champions do you have?")
     user_input_string = user_input_string.split(" ")
-
-    print(user_input_string)
-
     for i in user_input_string:
         if '-' in i:
             team_comps.delete_champ(i)
         else:
             team_comps.add_champ(i)
 
-    calculate.match(user_input, team_comps.tier_S)
+    result = calculate.match(user_input_string)
+    print("Recommended team(s) ----> %s\n" % result)
 
 
 while 1:
